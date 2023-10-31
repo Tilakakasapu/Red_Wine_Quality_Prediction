949bbe7186e3c1d547c58dcb6287cb224a9f3faa
Tilakakasapu <tilak57233@gmail.com>
2023-10-31 14:19:11 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Red_Wine_Quality_Prediction
+
+1. update config.yaml
+2. update schema.yaml
+3. update params.yaml
+4. update the entity
+5. update the configuration manager in the src config
+6. updatethe components
+7. update the pipelines
+8. update the main.py
+9. update the app.py
--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -5,16 +5,16 @@ from src.mlproject import logger
 import json 
 import joblib
 from ensure import ensure_annotations
-from box import ConfigBox
+from box.config_box import ConfigBox
 from pathlib import Path
 from typing import Any
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open (path_to_yaml) as yaml_file:
-            content = yaml.safe_load
+            content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file:{path_to_yaml} loaded successfully")
-            return ConfigBox
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
